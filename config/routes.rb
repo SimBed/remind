724fee40cde804be46dc93ec3775cd8ab7b27a0b
SimBed <dansimbed@gmail.com>
2026-01-30 13:48:50 +0000

    add warmup route to ping to keep app awake

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/warmup", to: proc { ActiveRecord::Base.connection.execute("SELECT 1")
+                            [ 200, {}, [ "OK" ] ]
+                          }, as: :ping_to_keep_app_awake
 end
